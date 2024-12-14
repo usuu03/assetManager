0e963e9d0f3759157b75949202a27d9c55624911
Usu <usuedeaghe@gmail.com>
2024-12-14 21:01:55 +0000

    updating organization model

--- asset_manager_backend/organization/models.py
+++ asset_manager_backend/organization/models.py
@@ -3,6 +3,12 @@ from django.db import models
 # Create your models here.
 class Organization(models.Model):
     name = models.CharField(max_length=255, null=False)
+    description = models.TextField(null=True, blank=True)
+    email = models.EmailField(unique=True, null=True, blank=True)
+    logo = models.ImageField(upload_to='organization_logos/', null=True, blank=True)
+    phone_number = models.CharField(max_length=25, null=True, blank=True)
+    website = models.URLField(unique=True, null=True, blank=True)
+    industry = models.CharField(max_length=100, null=True, blank=True)
    
     
     def __str__(self):
